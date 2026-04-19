b36774bf0d7613f45e63a0d357b0f44116ed0046
Shaan Singh <shaaninventions@gmail.com>
2026-04-18 23:00:43 -0400

    Extra Credit: Magic Square

--- src/FunWith2DArrays.java
+++ src/FunWith2DArrays.java
@@ -1,58 +1,58 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class FunWith2DArrays {
-    public static int totalElements(int[][] arr){
+    public static int totalElements(int[][] arr) {
         return arr.length * arr[0].length;
     }
 
-    public static void fourCorners(String[][] arr){
+    public static void fourCorners(String[][] arr) {
         System.out.println(arr[0][0]);
-        System.out.println(arr[0][arr[0].length -1]);
-        System.out.println(arr[arr.length -1][0]);
-        System.out.println(arr[arr.length -1][arr[arr.length -1].length -1]);
+        System.out.println(arr[0][arr[0].length - 1]);
+        System.out.println(arr[arr.length - 1][0]);
+        System.out.println(arr[arr.length - 1][arr[arr.length - 1].length - 1]);
     }
 
-    public static void swapFrontAndBackRows(String[][] arr){
+    public static void swapFrontAndBackRows(String[][] arr) {
         String[] temp = arr[0];
-        arr[0] = arr[arr.length -1];
-        arr[arr.length -1] = temp;
+        arr[0] = arr[arr.length - 1];
+        arr[arr.length - 1] = temp;
     }
 
-    public static double average(int[][]  ints){
+    public static double average(int[][] ints) {
         int total = 0;
-        for(int[] row: ints){
-            for(int num : row){
+        for (int[] row : ints) {
+            for (int num : row) {
                 total += num;
             }
         }
-        return (double) total/totalElements(ints);
+        return (double) total / totalElements(ints);
     }
 
-    public static int edgeSum(int[][] ints){
+    public static int edgeSum(int[][] ints) {
         int total = 0;
         for (int i = 0; i < ints[0].length; i++) {
-            total+= ints[0][i] + ints[ints.length -1][i];
+            total += ints[0][i] + ints[ints.length - 1][i];
         }
         for (int i = 1; i < ints.length - 1; i++) {
-            total += ints[i][0] + ints[i][ints[i].length -1];
+            total += ints[i][0] + ints[i][ints[i].length - 1];
         }
         return total;
     }
 
-    public static int[] indexFound(String[][] strings, String target){
+    public static int[] indexFound(String[][] strings, String target) {
         for (int col = 0; col < strings[0].length; col++) {
             for (int row = 0; row < strings.length; row++) {
-                if (strings[row][col].equals(target)){
+                if (strings[row][col].equals(target)) {
                     return new int[]{row, col};
                 }
             }
         }
-        return new int[] {-1, -1};
+        return new int[]{-1, -1};
     }
-    public static int[][] split(int[][] ints, int row, int column){
+
+    public static int[][] split(int[][] ints, int row, int column) {
         if (ints.length <= row || ints[0].length <= column) return new int[0][0];
-        int[][] result = new int[row +1][column + 1];
+        int[][] result = new int[row + 1][column + 1];
 
         for (int i = 0; i < row + 1; i++) {
             for (int j = 0; j < column + 1; j++) {
@@ -63,7 +63,7 @@ public class FunWith2DArrays {
         return result;
     }
 
-    public static int[][] invert(int[][] ints){
+    public static int[][] invert(int[][] ints) {
         int[][] arr = new int[ints[0].length][ints.length];
         for (int i = 0; i < ints.length; i++) {
             for (int j = 0; j < ints[0].length; j++) {
@@ -74,17 +74,17 @@ public class FunWith2DArrays {
         return arr;
     }
 
-    /** Return the sum of all elements of arr that are in the one particular row,
-     *  specified by the row parameter
+    /**
+     * Return the sum of all elements of arr that are in the one particular row,
+     * specified by the row parameter
+     * <p>
+     * PRECONDITION:  0 <= row < arr.length (i.e. row is guaranteed to be valid)
      *
-     *  PRECONDITION:  0 <= row < arr.length (i.e. row is guaranteed to be valid)
-     *
-     *  @param arr  2D array of ints
-     *  @param row  the row to add up
-     *  @return  the sum of all elements in row
+     * @param arr 2D array of ints
+     * @param row the row to add up
+     * @return the sum of all elements in row
      */
-    public static int sumForRow(int[][] arr, int row)
-    {
+    public static int sumForRow(int[][] arr, int row) {
         int sum = 0;
         for (int i = 0; i < arr[row].length; i++) {
             sum += arr[row][i];
@@ -92,16 +92,16 @@ public class FunWith2DArrays {
         return sum;
     }
 
-    /** Return the sum of all elements of arr that are in the one particular column
+    /**
+     * Return the sum of all elements of arr that are in the one particular column
+     * <p>
+     * PRECONDITION:  0 <= col < arr[0].length (i.e. col is valid)
      *
-     *  PRECONDITION:  0 <= col < arr[0].length (i.e. col is valid)
-     *
-     *  @param arr  2D array of ints
-     *  @param col  the column to add up
-     *  @return  the sum of all elements in column
+     * @param arr 2D array of ints
+     * @param col the column to add up
+     * @return the sum of all elements in column
      */
-    public static int sumForColumn(int[][] arr, int col)
-    {
+    public static int sumForColumn(int[][] arr, int col) {
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             sum += arr[i][col];
@@ -109,25 +109,25 @@ public class FunWith2DArrays {
         return sum;
     }
 
-    /** Replaces all even integers in arr with 0; all odd integers are unchanged.
-     *  It then returns the number of changes that were made.
-     *
-     *  Example: if arr is {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {4, 6, 8, 3, 5}}
-     *  then this method modifies arr to be:
-     *           {{1, 0, 3, 0, 5}, {0, 7, 0, 9, 0}, {0, 0, 0, 3, 5}}
-     *  and returns 8 (the number of even numbers replaced by 0)
-     *
+    /**
+     * Replaces all even integers in arr with 0; all odd integers are unchanged.
+     * It then returns the number of changes that were made.
+     * <p>
+     * Example: if arr is {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {4, 6, 8, 3, 5}}
+     * then this method modifies arr to be:
+     * {{1, 0, 3, 0, 5}, {0, 7, 0, 9, 0}, {0, 0, 0, 3, 5}}
+     * and returns 8 (the number of even numbers replaced by 0)
+     * <p>
      * POSTCONDITION: this method modifies the original 2D array referenced by arr
      *
-     *  @param arr  2D array of ints
-     *  @return  the number of changes made
+     * @param arr 2D array of ints
+     * @return the number of changes made
      */
-    public static int replaceEvensWithZero(int[][] arr)
-    {
+    public static int replaceEvensWithZero(int[][] arr) {
         int count = 0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
-                if (arr[i][j] % 2 == 0){
+                if (arr[i][j] % 2 == 0) {
                     arr[i][j] = 0;
                     count++;
                 }
@@ -136,20 +136,20 @@ public class FunWith2DArrays {
         return count;
     }
 
-    /** Searches through the 2D array wordChart and finds all strings with
-     *  length len; these strings are added to an ArrayList and returned.
-     *  If no strings have that length, return an empty ArrayList
+    /**
+     * Searches through the 2D array wordChart and finds all strings with
+     * length len; these strings are added to an ArrayList and returned.
+     * If no strings have that length, return an empty ArrayList
      *
-     *  @param wordChart  2D array of Strings
-     *  @param len  the length of strings to search for
-     *  @return  an ArrayList containing all strings in wordChart with length len
+     * @param wordChart 2D array of Strings
+     * @param len       the length of strings to search for
+     * @return an ArrayList containing all strings in wordChart with length len
      */
-    public static ArrayList<String> findStringsOfLength(String[][] wordChart, int len)
-    {
+    public static ArrayList<String> findStringsOfLength(String[][] wordChart, int len) {
         ArrayList<String> strings = new ArrayList<>();
         for (int i = 0; i < wordChart.length; i++) {
             for (int j = 0; j < wordChart[0].length; j++) {
-                if (wordChart[i][j].length() == len){
+                if (wordChart[i][j].length() == len) {
                     strings.add(wordChart[i][j]);
                 }
             }
@@ -157,17 +157,17 @@ public class FunWith2DArrays {
         return strings;
     }
 
-    /** calculates and returns the average class grade as a double for a
-     *  all Students in the 2D array seatingChart
+    /**
+     * calculates and returns the average class grade as a double for a
+     * all Students in the 2D array seatingChart
+     * <p>
+     * PRECONDITION:  seatingChart contains at least one element with at least
+     * one student (i.e. no need to worry about division by 0)
      *
-     *  PRECONDITION:  seatingChart contains at least one element with at least
-     *                 one student (i.e. no need to worry about division by 0)
-     *
-     *  @param seatingChart  2D array of Student objects
-     *  @return  the average grade of all Students in seatingChart, as a double
+     * @param seatingChart 2D array of Student objects
+     * @return the average grade of all Students in seatingChart, as a double
      */
-    public static double classAverage(Student[][] seatingChart)
-    {
+    public static double classAverage(Student[][] seatingChart) {
         int total = 0;
         for (int i = 0; i < seatingChart.length; i++) {
             for (int j = 0; j < seatingChart[0].length; j++) {
@@ -175,57 +175,57 @@ public class FunWith2DArrays {
             }
         }
 
-        return (double) total/(seatingChart.length  * seatingChart[0].length);
+        return (double) total / (seatingChart.length * seatingChart[0].length);
     }
 
-    /** Returns true if any two consecutive elements, horizontally or vertically,
-     *  are equal, false otherwise.  You do not need to check for "wrap around"
-     *  consecutiveness, such as if the last element in a row is the same as
-     *  the first:
+    /**
+     * Returns true if any two consecutive elements, horizontally or vertically,
+     * are equal, false otherwise.  You do not need to check for "wrap around"
+     * consecutiveness, such as if the last element in a row is the same as
+     * the first:
+     * <p>
+     * For example, if arr is:
+     * 1, 2, 3, 4
+     * 2, 7, 3, 5
+     * 3, 4, 5, 6
+     * <p>
+     * then this method returns true because there is a pair of consecutive
+     * numbers (vertically)
+     * <p>
+     * As another example, if arr is:
+     * 1, 2, 3, 4
+     * 2, 7, 8, 5
+     * 3, 4, 4, 6
+     * <p>
+     * then this method returns true because there is a pair of consecutive
+     * numbers (horizontally)
+     * <p>
+     * As another example, if arr is:
+     * 1, 2, 3, 8
+     * 2, 7, 8, 5
+     * 3, 2, 5, 3
+     * <p>
+     * then this method returns false because there are not two consecutive
+     * numbers, vertically or horizontally, that are equal.
+     * <p>
+     * NOTE: do not consider "wrap around" consecutive numbers, in other words,
+     * the 3's and 6's below are not considered consecutive:
+     * 1, 6, 3, 8
+     * 2, 7, 8, 5
+     * 3, 6, 5, 3
+     * <p>
+     * Similarly, equal numbers on a diagonal are not considered consecutive,
+     * so the 8's below are not consecutive:
+     * 1, 2, 3, 8
+     * 2, 7, 8, 5
+     * 3, 2, 5, 3
      *
-     *  For example, if arr is:
-     *      1, 2, 3, 4
-     *      2, 7, 3, 5
-     *      3, 4, 5, 6
-     *
-     *  then this method returns true because there is a pair of consecutive
-     *  numbers (vertically)
-     *
-     *  As another example, if arr is:
-     *      1, 2, 3, 4
-     *      2, 7, 8, 5
-     *      3, 4, 4, 6
-     *
-     *  then this method returns true because there is a pair of consecutive
-     *  numbers (horizontally)
-     *
-     *  As another example, if arr is:
-     *      1, 2, 3, 8
-     *      2, 7, 8, 5
-     *      3, 2, 5, 3
-     *
-     *  then this method returns false because there are not two consecutive
-     *  numbers, vertically or horizontally, that are equal.
-     *
-     *  NOTE: do not consider "wrap around" consecutive numbers, in other words,
-     *  the 3's and 6's below are not considered consecutive:
-     *      1, 6, 3, 8
-     *      2, 7, 8, 5
-     *      3, 6, 5, 3
-     *
-     *  Similarly, equal numbers on a diagonal are not considered consecutive,
-     *  so the 8's below are not consecutive:
-     *      1, 2, 3, 8
-     *      2, 7, 8, 5
-     *      3, 2, 5, 3
-     *
-     *  @param arr  2D array of ints
+     * @param arr 2D array of ints
      */
-    public static boolean consecutive(int[][] arr)
-    {
+    public static boolean consecutive(int[][] arr) {
         int[][] arr2 = new int[arr.length + 2][arr[0].length + 2];
 
-        for (int i = 0; i < arr.length ; i++) {
+        for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
                 arr2[i + 1][j + 1] = arr[i][j];
             }
@@ -233,21 +233,104 @@ public class FunWith2DArrays {
 
         for (int i = 1; i < arr2.length - 1; i++) {
             for (int j = 1; j < arr2[0].length - 1; j++) {
-                int up = arr2[i-1][j];
-                int down = arr2[i+1][j];
+                int up = arr2[i - 1][j];
+                int down = arr2[i + 1][j];
                 int left = arr2[i][j - 1];
-                int right = arr2[i][j +1];
+                int right = arr2[i][j + 1];
                 int currentPosition = arr2[i][j];
-                if(currentPosition == up || currentPosition == down || currentPosition == left || currentPosition == right) return true;
+                if (currentPosition == up || currentPosition == down || currentPosition == left || currentPosition == right)
+                    return true;
             }
 
         }
         return false;
     }
 
+    /**
+     * A "magic square" is a square grid filled with distinct integers such
+     * that each element is a different integer, and the sum of the integers
+     * in each row, column and diagonal is equal.  This method should return true
+     * if arr is a magic square.
+     * <p>
+     * PRECONDITION: arr is a square 2D array with the same number of rows and
+     * columns
+     * <p>
+     * For example, if arr is:
+     * 7, 2, 3
+     * 0, 4, 8
+     * 5, 6, 1
+     * <p>
+     * then this method returns true because each row, column, and diagonal add
+     * up to the same sum (12) and all numbers are unique (i.e. no duplicate
+     * values appear)
+     * <p>
+     * As another example, if arr is the following (note the 3 and 2 swapped):
+     * 7, 3, 2
+     * 0, 4, 8
+     * 5, 6, 1
+     * <p>
+     * then this method returns false because the column sums no longer add up to
+     * the same value
+     * <p>
+     * As another example, if arr is:
+     * 1, 3, 2
+     * 3, 2, 1
+     * 2, 1, 3
+     * <p>
+     * then this method returns false because, although the rows, columns, and
+     * diagonals add up to the same sum (6), there are duplicate numbers.
+     *
+     * @param arr 2D array of ints
+     */
+    public static boolean magicSquare(int[][] arr) {
+        ArrayList<Integer> seenNums = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                for (int num : seenNums) {
+                    if (num == arr[i][j]) {
+                        return false;
+                    }
+                }
+                seenNums.add(arr[i][j]);
+            }
+        }
+        int totalSum = 0;
+        //Find sum value of Square
+        for (int i = 0; i < arr.length; i++) {
+            totalSum += arr[0][i];
+        }
 
+        //Check Rows
+        for (int i = 0; i < arr.length; i++) {
+            int rowSum = 0;
+            for (int j = 0; j < arr.length; j++) {
+                rowSum += arr[i][j];
+            }
+            if (rowSum != totalSum) return false;
+        }
 
+        //Check Columns
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < arr.length; j++) {
+                sum += arr[j][i];
+            }
+            if (sum != totalSum) return false;
+        }
 
+        //Check Diagonals
+
+        int diagonalSum = 0;
+        int diagonalSum2 = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            diagonalSum += arr[i][i];
+            diagonalSum2 += arr[arr.length - i - 1][arr.length - i - 1];
+        }
+        if (diagonalSum != totalSum || diagonalSum2 != totalSum) return false;
+
+        return true;
+    }
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1068,86 +1068,141 @@ public class Main {
 //            System.out.println("!!!!!!! TEST FAILS !!!!!!!");
 //        }
 
-        int[][] testArr1 = { // true
-                {1, 2, 3, 4},
-                {1, 3, 4, 5},
-                {3, 4, 5, 6}
-        };
-        int[][] testArr2 = { // true
-                {1, 2, 3, 4},
-                {2, 3, 4, 6},
-                {3, 4, 5, 6}
-        };
-        int[][] testArr3 = { // true
-                {7, 7, 3, 4},
-                {2, 3, 4, 5},
-                {3, 4, 5, 6}
-        };
-        int[][] testArr4 = { // true
-                {1, 2, 3, 4},
-                {2, 3, 4, 5},
-                {3, 4, 2, 2}
-        };
-        int[][] testArr5 = { // true
-                {1, 2, 3, 4, 5},
-                {2, 3, 9, 5, 6},
-                {3, 4, 9, 6, 7},
-                {4, 5, 6, 7, 8}
-        };
-        int[][] testArr6 = { // true
-                {1, 2, 3, 4, 5},
-                {2, 3, 4, 5, 6},
-                {3, 4, 1, 1, 7},
-                {4, 5, 6, 7, 8}
-        };
-        int[][] testArr7 = { // true
-                {1, 2, 3, 4, 5},
-                {2, 3, 4, 5, 6},
-                {3, 4, 5, 6, 8},
-                {4, 5, 6, 7, 8}
-        };
-        int[][] testArr8 = { // true
-                {1, 2, 3, 4, 5},
-                {2, 3, 4, 5, 6},
-                {3, 4, 5, 6, 7},
-                {4, 5, 6, 8, 8}
-        };
-        int[][] testArr9 = { // false
-                {1, 2, 3, 4},
-                {2, 3, 4, 5},
-                {3, 4, 5, 6}
-        };
-        int[][] testArr10 = { // false
-                {1, 2, 3, 4, 5},
-                {2, 3, 4, 5, 6},
-                {3, 4, 5, 6, 7},
-                {4, 5, 6, 7, 8}
-        };
-        boolean test1 = FunWith2DArrays.consecutive(testArr1);
-        System.out.println(test1);
-        boolean test2 = FunWith2DArrays.consecutive(testArr2);
-        System.out.println(test2);
-        boolean test3 = FunWith2DArrays.consecutive(testArr3);
-        System.out.println(test3);
-        boolean test4 = FunWith2DArrays.consecutive(testArr4);
-        System.out.println(test4);
-        boolean test5 = FunWith2DArrays.consecutive(testArr5);
-        System.out.println(test5);
-        boolean test6 = FunWith2DArrays.consecutive(testArr6);
-        System.out.println(test6);
-        boolean test7 = FunWith2DArrays.consecutive(testArr7);
-        System.out.println(test7);
-        boolean test8 = FunWith2DArrays.consecutive(testArr8);
-        System.out.println(test8);
-        boolean test9 = FunWith2DArrays.consecutive(testArr9);
-        System.out.println(test9);
-        boolean test10 = FunWith2DArrays.consecutive(testArr10);
-        System.out.println(test10);
-        if (test1 && test2 && test3 && test4 && test5 && test6 && test7 && test8 && !test9 && !test10) {
-            System.out.println("\n--- ALL TESTS PASS! ---");
+//        int[][] testArr1 = { // true
+//                {1, 2, 3, 4},
+//                {1, 3, 4, 5},
+//                {3, 4, 5, 6}
+//        };
+//        int[][] testArr2 = { // true
+//                {1, 2, 3, 4},
+//                {2, 3, 4, 6},
+//                {3, 4, 5, 6}
+//        };
+//        int[][] testArr3 = { // true
+//                {7, 7, 3, 4},
+//                {2, 3, 4, 5},
+//                {3, 4, 5, 6}
+//        };
+//        int[][] testArr4 = { // true
+//                {1, 2, 3, 4},
+//                {2, 3, 4, 5},
+//                {3, 4, 2, 2}
+//        };
+//        int[][] testArr5 = { // true
+//                {1, 2, 3, 4, 5},
+//                {2, 3, 9, 5, 6},
+//                {3, 4, 9, 6, 7},
+//                {4, 5, 6, 7, 8}
+//        };
+//        int[][] testArr6 = { // true
+//                {1, 2, 3, 4, 5},
+//                {2, 3, 4, 5, 6},
+//                {3, 4, 1, 1, 7},
+//                {4, 5, 6, 7, 8}
+//        };
+//        int[][] testArr7 = { // true
+//                {1, 2, 3, 4, 5},
+//                {2, 3, 4, 5, 6},
+//                {3, 4, 5, 6, 8},
+//                {4, 5, 6, 7, 8}
+//        };
+//        int[][] testArr8 = { // true
+//                {1, 2, 3, 4, 5},
+//                {2, 3, 4, 5, 6},
+//                {3, 4, 5, 6, 7},
+//                {4, 5, 6, 8, 8}
+//        };
+//        int[][] testArr9 = { // false
+//                {1, 2, 3, 4},
+//                {2, 3, 4, 5},
+//                {3, 4, 5, 6}
+//        };
+//        int[][] testArr10 = { // false
+//                {1, 2, 3, 4, 5},
+//                {2, 3, 4, 5, 6},
+//                {3, 4, 5, 6, 7},
+//                {4, 5, 6, 7, 8}
+//        };
+//        boolean test1 = FunWith2DArrays.consecutive(testArr1);
+//        System.out.println(test1);
+//        boolean test2 = FunWith2DArrays.consecutive(testArr2);
+//        System.out.println(test2);
+//        boolean test3 = FunWith2DArrays.consecutive(testArr3);
+//        System.out.println(test3);
+//        boolean test4 = FunWith2DArrays.consecutive(testArr4);
+//        System.out.println(test4);
+//        boolean test5 = FunWith2DArrays.consecutive(testArr5);
+//        System.out.println(test5);
+//        boolean test6 = FunWith2DArrays.consecutive(testArr6);
+//        System.out.println(test6);
+//        boolean test7 = FunWith2DArrays.consecutive(testArr7);
+//        System.out.println(test7);
+//        boolean test8 = FunWith2DArrays.consecutive(testArr8);
+//        System.out.println(test8);
+//        boolean test9 = FunWith2DArrays.consecutive(testArr9);
+//        System.out.println(test9);
+//        boolean test10 = FunWith2DArrays.consecutive(testArr10);
+//        System.out.println(test10);
+//        if (test1 && test2 && test3 && test4 && test5 && test6 && test7 && test8 && !test9 && !test10) {
+//            System.out.println("\n--- ALL TESTS PASS! ---");
+//        } else {
+//            System.out.println("\n!!!!!! TEST FAIL!!!!!!!");
+//        }
+
+
+        int[][] testArr1 = {{7, 2, 3}, {0, 4, 8}, {5, 6, 1}};
+        boolean test1 = FunWith2DArrays.magicSquare(testArr1);
+        System.out.println(test1 + " --> 1. magic square!");
+        int[][] testArr2 = {{4, 9, 2}, {3, 5, 7}, {8, 1, 6}};
+        boolean test2 = FunWith2DArrays.magicSquare(testArr2);
+        System.out.println(test2 + " --> 2. magic square!");
+        int[][] testArr3 = {{2, 16, 13, 3}, {11, 5, 8, 10}, {7, 9, 12, 6}, {14, 4, 1, 15}};
+        boolean test3 = FunWith2DArrays.magicSquare(testArr3);
+        System.out.println(test3 + " --> 3. magic square!");
+        int[][] testArr4 = {{1, 23, 16, 4, 21}, {15, 14, 7, 18, 11}, {24, 17, 13, 9, 2}, {20, 8, 19, 12, 6}, {5, 3, 10, 22, 25}};
+        boolean test4 = FunWith2DArrays.magicSquare(testArr4);
+        System.out.println(test4 + " --> 3. magic square!");
+        System.out.println("-------");
+        int[][] testArr8 = {{3, 3, 3}, {3, 3, 3}, {3, 3, 3}};
+        boolean test8 = FunWith2DArrays.magicSquare(testArr8);
+        System.out.println(test8 + " --> 4. duplicate values");
+        int[][] testArr9 = {{1, 3, 2}, {3, 2, 1}, {2, 1, 3}};
+        boolean test9 = FunWith2DArrays.magicSquare(testArr9);
+        System.out.println(test9 + " --> 5. duplicate values");
+        int[][] testArr14 = {{2, 8, 5}, {8, 5, 2}, {5, 2, 8}};
+        boolean test14 = FunWith2DArrays.magicSquare(testArr14);
+        System.out.println(test14 + " --> 6. duplicate values");
+        int[][] testArr15 = {{4, 5, 6}, {7, 5, 3}, {4, 5, 6}};
+        boolean test15 = FunWith2DArrays.magicSquare(testArr15);
+        System.out.println(test15 + " --> 7. duplicate values");
+        int[][] testArr6 = {{2, 16, 13, 3}, {11, 5, 8, 10}, {7, 9, 12, 6}, {14, 4, 1, 8}};
+        boolean test6 = FunWith2DArrays.magicSquare(testArr6);
+        System.out.println(test6 + " --> 8. duplicate value");
+        int[][] testArr5 = {{2, 16, 13, 3}, {11, 5, 8, 10}, {7, 9, 12, 15}, {14, 4, 1, 6}};
+        boolean test5 = FunWith2DArrays.magicSquare(testArr5);
+        System.out.println(test5 + " --> 9. row/col/diag sums not equal");
+        int[][] testArr7 = {{7, 3, 2}, {0, 4, 8}, {5, 6, 1}};
+        boolean test7 = FunWith2DArrays.magicSquare(testArr7);
+        System.out.println(test7 + " --> 10. row/col/diag sums not equal");
+        int[][] testArr10 = {{0, 4, 8}, {7, 2, 3}, {5, 6, 1}};
+        boolean test10 = FunWith2DArrays.magicSquare(testArr10);
+        System.out.println(test10 + " --> 11. row/col/diag sums not equal");
+        int[][] testArr11 = {{1, 23, 16, 4, 21}, {15, 14, 7, 18, 11}, {24, 26, 13, 9, 2}, {20, 8, 19, 12, 6}, {5, -6, 10, 22, 25}};
+        boolean test11 = FunWith2DArrays.magicSquare(testArr11);
+        System.out.println(test11 + " --> 12. row/col/diag sums not equal");
+        int[][] testArr12 = {{1, 23, 16, 4, 21}, {15, 14, 7, 18, 11}, {24, 27, 13, -1, 2}, {20, 8, 19, 12, 6}, {5, 3, 10, 22, 25}};;
+        boolean test12 = FunWith2DArrays.magicSquare(testArr12);
+        System.out.println(test12 + " --> 13. row/col/diag sums not equal");
+        int[][] testArr13 = {{1, 2, 9}, {4, 15, -7}, {7, -5, 10}};
+        boolean test13 = FunWith2DArrays.magicSquare(testArr13);
+        System.out.println(test13 + " --> 14. row/col/diag sums not equal");
+        System.out.println("-------");
+        if (test1 && test2 && test3 && test4 && !test5 && !test6 && !test7 && !test8 && !test9 && !test10 && !test11 && !test12 && !test13) {
+            System.out.println("ALL TESTS PASS!");
         } else {
-            System.out.println("\n!!!!!! TEST FAIL!!!!!!!");
+            System.out.println("!!!!!!!! TEST FAILS !!!!!!!!!");
         }
+
 
 
 
